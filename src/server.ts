62e81bf75b71435bf492dbe154323f56b49481b0
Vidal1402 <luisguilhermevidal98@gmail.com>
2026-02-04 13:17:42 -0300

    cors conrrigido

--- src/server.ts
+++ src/server.ts
@@ -40,6 +40,7 @@ export function buildServer() {
   app.register(cors, {
     origin: true,
     credentials: false,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
   });
 
